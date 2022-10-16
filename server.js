7fb28352001e752afbc3381b6dd4a96c793919ff
Fahad <fahad.fadley@gmail.com>
2022-10-16 16:27:42 +0300

    tt

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.post("/create", upload.single('file'), function(req, res) {
 
 app.post("/delete", function(req, res) {
   const ds = req.body.dataset
+  console.log(ds);
   deleteDataset(ds, currentUser)
   res.redirect("/homepage");
 })
